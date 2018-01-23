0a49c656c0b98549ef869bd7c6f21fb47a7d6d60
LOUIS GARON - STUDENT <G00346194@gmit.ie>
2018-01-23 17:23:58 +0000

    new Readme2 added

---
+++ basic_types.ts
@@ -0,0 +1,25 @@
+//boolean
+let flag: boolean = true;
+console.log("Value assigned to flag is: " +flag);
+
+//number
+let num: number = 6;
+console.log("Value assigned to num is: " +num);
+
+//String
+let hello: string = "Hello World!!!";
+console.log("Value assigned to hello is: " +hello);
+
+//Array
+let array: number[] = [1,2,3];
+console.log("Value assinged to array is:" + array);
+
+//Tuple
+let x: [string ,number];
+x = ["I am", 21];
+console.log(x[0].substr(1));
+
+//Enum
+enum Color {Red, Green, Blue}
+let c: Color = Color.Blue;
+
